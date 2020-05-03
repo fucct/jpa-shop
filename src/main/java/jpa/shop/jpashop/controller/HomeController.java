2f8e93a024799af1c4559229bdcb9113c3c5cbb6
dd <dqrd123@gmail.com>
2020-05-04 07:33:04 +0900

    feat : 웹 메인 화면 구현
    - HomeController
    - Home 페이지

---
+++ src/main/java/jpa/shop/jpashop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpa.shop.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j  //logger
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
---
+++ src/main/java/jpa/shop/jpashop/domain/OrderSearch.java
@@ -0,0 +1,11 @@
+package jpa.shop.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private String orderStatus;
+}
